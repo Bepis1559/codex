af0354a89e057c75f7d035b6a979fd0c63c223e7
Bepis1559 <axl2001@abv.bg>
2023-03-02 20:55:33 +0200

    adding cors stuff

--- server/server.js
+++ server/server.js
@@ -13,6 +13,13 @@ const openai = new OpenAIApi(configuration);
 
 const app = express()
 app.use(cors())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.use(express.json())
 
 app.get('/', async (req, res) => {
